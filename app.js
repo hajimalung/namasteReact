20115e63845684e6c9bf37ab1949e846adb81792
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-09-19 16:24:47 +0530

    creating elements inside elements in react

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const heading = React.createElement("h1",{ id : "heading"},"Hello world from rea
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1> this is h1 tag </h1>
+ *      </div>
+ * </div>
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    { id: "parent"},
+    React.createElement( 
+        "div",
+        { id: "child" },
+        React.createElement(
+            "h1",
+            {},
+            "I am an h1 tag!")
+    )
+);
+
+root.render(parent);
