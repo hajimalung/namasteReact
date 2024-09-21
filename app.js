035322b883d9d80b0db1fb1c5430066c41ffe080
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-09-21 13:44:56 +0530

    Functional component
    jsx multiline and arrrow function way of react component

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ const heading = React.createElement(
 // create react element using JSX
 // JSX - not html in javascript
 // JSX is html like or XML like syntax - but not html
-const jsxHeading = <h1 id="heading">Namaste React usin JSX 🔥 🚀</h1>
+
+// attributes must be in camelCase , explore more jsx attributes and elements
+// if we have to write in multiple lines in JSX wrap inside ()
+
+const jsxHeading = (
+    <h1 id="heading" className="root" tabIndex="1" >
+        Namaste React usin JSX 🔥 🚀
+    </h1>
+    );
+
 // console.log(jsxHeading); // jsxheading is a react elemet
 // jsx 
 
@@ -21,6 +30,22 @@ const jsxHeading = <h1 id="heading">Namaste React usin JSX 🔥 🚀</h1>
 
 // parcel "the beast" transpiles the jsx code to pure js that browser understands using babel
 // before it reaches the js engine
+// babel converts jsx to react core 
+
+
+// React Component
+// 1. class based components - OLD - js classes
+
+// 2. functional componennts - New - js functions to create components
+// js function that returns react/JSX element is functional react component
+// name it with Capital Letter
+
+const HeadingComponent = ()=> (<h1 className="header" tabIndex="1">Namaste react functional component! </h1>);
+
+const HeadingComponent2 = ()=> {
+    return <h1 className="header" tabIndex="1">Namaste react functional component! </h1>;
+};
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
